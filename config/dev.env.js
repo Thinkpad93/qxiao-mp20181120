431141a3dbc80317355c1b9af03c9feeb7a8c7be
ThinkPad <1084677348@qq.com>
2019-09-29 09:42:25 +0800

    完善手环页面

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,4 +7,5 @@ module.exports = merge(prodEnv, {
   //BASE_API: '"http://192.168.18.199:8080/"'
   BASE_API: '"http://192.168.18.200:8080/"'
   // BASE_API: '"http://qxiao.natapp1.cc/"'
+  //BASE_API: '"http://192.168.0.106:8080/"'
 });
